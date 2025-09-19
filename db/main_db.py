7a7c5407120082935c37bd94dfe811a1f1de18dd
ch1ng1z10z <ggckam228mail.ru>
2025-09-19 15:04:33 +0600

    -a

--- db/main_db.py
+++ db/main_db.py
@@ -2,14 +2,16 @@ import sqlite3
 from db import queries
 from config import path_db
 
+
 def init_db():
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.executequ(queries.CREATE_TABLE_TASKS)
-    print("база даныых")
+    cursor.execute(queries.CREATE_TABLE_TASKS)
+    print("База данных подключена!")
     conn.commit()
     conn.close()
-    
+
+
 def get_tasks():
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
@@ -17,11 +19,12 @@ def get_tasks():
     tasks = cursor.fetchall()
     conn.close()
     return tasks
-    
+
+
 def add_task(task):
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.execute(queries.SELECT_TASKS,(task,) )
+    cursor.execute(queries.INSERT_TASK, (task,))
     conn.commit()
     task_id = cursor.lastrowid
     conn.close()
@@ -31,15 +34,14 @@ def add_task(task):
 def update_task(task_id, new_task):
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.execute(queries.UPDATE_TASKS, (new_task, task_id))
+    cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
     conn.commit()
     conn.close()
-
 
 
 def delete_task(task_id):
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.execute(queries.DELETE_TASK,(task_id))
+    cursor.execute(queries.DELETE_TASK, (task_id, ))
     conn.commit()
     conn.close()
